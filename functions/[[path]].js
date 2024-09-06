6db8b0542513ecc86141db6ef0083fa73a9b2b39
Gus Escola <camagrock@gmail.com>
2024-09-06 11:21:55 +0200

    debugging

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,14 +1,21 @@
+// export default {
+//   async fetch(request, env, ctx) {
+//     const allowedIP = '85.50.8.64'; // Cambia esta IP por la que quieras permitir
+//     const clientIP = request.headers.get('CF-Connecting-IP') || '127.0.0.1'; // Simulamos IP localmente si no está disponible
+
+//     console.log(`IP del visitante: ${clientIP}`);
+
+//     if (clientIP !== allowedIP) {
+//       return new Response('Acceso denegado. Tu IP no tiene permiso para acceder a este sitio.', { status: 403 });
+//     }
+
+//     return new Response('Acceso permitido', { status: 200 });
+//   }
+// };
+
 export default {
-  async fetch(request, env, ctx) {
-    const allowedIP = '85.50.8.64'; // Cambia esta IP por la que quieras permitir
-    const clientIP = request.headers.get('CF-Connecting-IP') || '127.0.0.1'; // Simulamos IP localmente si no está disponible
-
-    console.log(`IP del visitante: ${clientIP}`);
-
-    if (clientIP !== allowedIP) {
-      return new Response('Acceso denegado. Tu IP no tiene permiso para acceder a este sitio.', { status: 403 });
-    }
-
-    return new Response('Acceso permitido', { status: 200 });
+  async fetch(request) {
+    return new Response('Hello from Worker!', { status: 200 });
   }
 };
+
